3d3cd22888d889e312c8d62dd6e50ea4d7892ca6
Michał Dobranowski <mdbrnowski@gmail.com>
2023-10-09 17:27:41 +0200

    Add an adequate interpretation of the arguments
    up to 1.15

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -2,17 +2,21 @@ package agh.ics.oop;
 
 public class World {
     public static void main(String[] args) {
-        System.out.println("System wystartował");
+        System.out.println("Start");
         run(args);
-        System.out.println("System zakończył działanie");
+        System.out.println("Stop");
     }
     public static void run(String[] args) {
-        System.out.println("Zwierzak idzie do przodu");
-        for (int i = 0; i < args.length; i++) {
-            System.out.print(args[i]);
-            if (i < args.length - 1)
-                System.out.print(", ");
+        for (String arg : args) {
+            String text = switch (arg) {
+                case "f" -> "Zwierzak idzie do przodu";
+                case "b" -> "Zwierzak idzie do tyłu";
+                case "r" -> "Zwierzak skręca w prawo";
+                case "l" -> "Zwierzak skręca w lewo";
+                default -> "";
+            };
+            System.out.println(text);
         }
-        System.out.println();
+
     }
 }
