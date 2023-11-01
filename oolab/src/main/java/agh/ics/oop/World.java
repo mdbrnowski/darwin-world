8d4619039c5dbbd14652f233412344a53d33c397
Michał Dobranowski <mdbrnowski@gmail.com>
2023-11-01 11:57:18 +0100

    Update World class

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,15 +1,15 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.MoveDirection;
-import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.*;
 
 import java.util.List;
 
 public class World {
     public static void main(String[] args) {
+        WorldMap map = new RectangularMap(4, 4);
         List<MoveDirection> directions = OptionsParser.parse(args);
         List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
-        Simulation simulation = new Simulation(directions, positions);
+        Simulation simulation = new Simulation(map, positions, directions);
         simulation.run();
     }
 
