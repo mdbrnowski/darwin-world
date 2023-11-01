ca9e0e1b65cd11a3a9581c7cbe183c91edc99143
Michał Dobranowski <mdbrnowski@gmail.com>
2023-11-01 11:50:19 +0100

    Add WorldMap and RectangularMap

---
+++ oolab/src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -0,0 +1,52 @@
+package agh.ics.oop.model;
+
+import agh.ics.oop.model.util.MapVisualizer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RectangularMap implements WorldMap {
+    private final int width;
+    private final int height;
+    private final Map<Vector2d, Animal> animals;
+
+    public RectangularMap(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.animals = new HashMap<>();
+    }
+
+    public boolean place(Animal animal) {
+        if (canMoveTo(animal.getPosition())) {
+            animals.put(animal.getPosition(), animal);
+            return true;
+        }
+        return false;
+    }
+
+    public void move(Animal animal, MoveDirection direction) {
+        Vector2d old_position = animal.getPosition();
+        animal.move(direction, this);
+        if (!animal.isAt(old_position)) {
+            animals.remove(old_position);
+            animals.put(animal.getPosition(), animal);
+        }
+    }
+
+    public boolean isOccupied(Vector2d position) {
+        return animals.get(position) != null;
+    }
+
+    public Animal objectAt(Vector2d position) {
+        return animals.get(position);
+    }
+
+    public boolean canMoveTo(Vector2d position) {
+        return 0 <= position.getX() && position.getX() <= width && 0 <= position.getY() &&
+                position.getY() <= height && !isOccupied(position);
+    }
+
+    public String toString() {
+        return new MapVisualizer(this).draw(new Vector2d(0, 0), new Vector2d(width, height));
+    }
+}
