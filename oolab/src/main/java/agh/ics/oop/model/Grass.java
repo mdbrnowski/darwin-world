0610cbce2c4c19cb35f97897bc8a2363e4a688e6
Michał Dobranowski <mdbrnowski@gmail.com>
2023-11-09 09:32:38 +0100

    Create WorldElement interface

---
+++ oolab/src/main/java/agh/ics/oop/model/Grass.java
@@ -0,0 +1,24 @@
+package agh.ics.oop.model;
+
+public class Grass implements WorldElement {
+    private final Vector2d position;
+
+    public Grass(Vector2d position) {
+        this.position = position;
+    }
+
+    @Override
+    public Vector2d getPosition() {
+        return position;
+    }
+
+    @Override
+    public boolean isAt(Vector2d position) {
+        return this.position.equals(position);
+    }
+
+    @Override
+    public String toString() {
+        return "*";
+    }
+}
--- oolab/src/main/java/agh/ics/oop/model/WorldMap.java
+++ oolab/src/main/java/agh/ics/oop/model/WorldMap.java
@@ -40,5 +40,5 @@ public interface WorldMap<T, P> extends MoveValidator<P> {
      * @param position The position of the animal.
      * @return animal or null if the position is not occupied.
      */
-    T objectAt(P position);
+    WorldElement objectAt(P position);
 }
