dd56b936dc915182350bde0fd6c78876e483a580
Michał Dobranowski <mdbrnowski@gmail.com>
2023-12-07 14:01:33 +0100

    Make RandomIterator anonymous

--- oolab/src/main/java/agh/ics/oop/model/util/RandomGenerator.java
+++ oolab/src/main/java/agh/ics/oop/model/util/RandomGenerator.java
@@ -5,33 +5,41 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
-//generates random array of non-repeating numbers
-public class RandomGenerator implements  Iterable<Integer> {
-    private List<Integer> unused=new ArrayList<Integer>();
-    private Random random;
+/**
+ * generates random array of non-repeating numbers
+ */
+public class RandomGenerator implements Iterable<Integer> {
+    private final List<Integer> unused = new ArrayList<>();
 
-    private List<Integer> used=new ArrayList<Integer>();
+    private final List<Integer> used = new ArrayList<>();
 
 
-    public RandomGenerator(int genomeSize,  int mutationCount) {
-
-        random=new Random();
-        for(int i=0;i<genomeSize;i++){
+    public RandomGenerator(int genomeSize, int mutationCount) {
+        Random random = new Random();
+        for (int i = 0; i < genomeSize; i++) {
             unused.add(i);
         }
 
-        for(int i=0;i<mutationCount;i++){
-            int size=unused.size();
-            int idx=random.nextInt(size);
-            int newPosition=unused.remove(idx);
+        for (int i = 0; i < mutationCount; i++) {
+            int size = unused.size();
+            int idx = random.nextInt(size);
+            int newPosition = unused.remove(idx);
             used.add(newPosition);
         }
-
-
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return new RandomIterator(used);
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return !used.isEmpty();
+            }
+
+            @Override
+            public Integer next() {
+                return used.remove(0);
+            }
+        };
     }
 }
