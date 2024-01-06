f7e39acb616f038b26368329f0f8311b67ea4ccd
ustankie <ustankiewicz@student.agh.edu.pl>
2024-01-06 17:40:11 +0100

    Add map tests

---
+++ oolab/src/test/java/SimulationTest.java
@@ -0,0 +1,31 @@
+import agh.ics.oop.Simulation;
+import agh.ics.oop.model.Animal;
+import agh.ics.oop.model.EarthGlobe;
+import agh.ics.oop.parameters.*;
+import agh.ics.oop.parameters.types.GenomeType;
+import agh.ics.oop.parameters.types.VegetationType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimulationTest {
+    @Test
+    public void constructorTest() {
+        GeneralParameters generalParameters = new GeneralParameters(GenomeType.FULL_PREDESTINATION_GENOME,
+                3, VegetationType.FOREST_EQUATORS, 12, 10);
+        EnergyParameters energyParameters = new EnergyParameters(3, 5,
+                3, 1);
+        MutationParameters mutationParameters = new MutationParameters("FullyRandom",
+                2, 3);
+        EarthGlobe map = new EarthGlobe(5, 7);
+        SimulationParameters parameters = new SimulationParameters(generalParameters, energyParameters, mutationParameters);
+        Simulation simulation = new Simulation(map, parameters);
+
+        Assertions.assertEquals(map.getPlants().size(), 12);
+        Assertions.assertEquals(map.getAnimals().size(), 10);
+
+        for (Animal animal : map.getAnimals()) {
+            Assertions.assertEquals(animal.getEnergy(), 5);
+        }
+    }
+
+}
