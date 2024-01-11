cc960c41c02c0380da1c2ae2f9169e1bd24dc864
Michał Dobranowski <mdbrnowski@gmail.com>
2024-01-11 15:29:47 +0100

    Handling invalid parameters (#38)
    * Add InvalidParametersException
    
    * Show invalid parameters in GUI
    
    * Fix energyForChild
    
    * Create separate ParametersValidator class

---
+++ oolab/src/main/java/agh/ics/oop/model/util/InvalidParametersException.java
@@ -0,0 +1,7 @@
+package agh.ics.oop.model.util;
+
+public class InvalidParametersException extends Exception {
+    public InvalidParametersException(String errorMessage) {
+        super(errorMessage);
+    }
+}
